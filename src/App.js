b2d9e4d08121886ec7bc7e0c3f25a5eb025fe8d7
Esteban Barac <EstebanBarac8@gmail.com>
2022-06-06 22:44:14 -0300

    ItemCount

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import ItemCount from './components/ItemCount.jsx';
 import ItemListContainer from './components/ItemListContainer.jsx';
 import  NavBar from './components/NavBar.jsx';
 
 
 function App() {
+
+  const onAdd = (count) => {
+    alert(`sumaste ${count} productos al carrito`);
+  }
+
   return (
     <>
         <NavBar />
         <ItemListContainer greeting={'Buenas Tardes'} />
+        <ItemCount initial={1} stock={9} onAdd ={onAdd}/>
     </> 
   );
 }
---
+++ src/components/ItemCount.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+
+export default function ItemCount({initial,  stock, onAdd}) {
+    const[count, setCount] = useState(initial)
+
+    const sumar = () => {
+        count < stock ? setCount(count + 1) : alert('No puedes agregar mas')     
+    }
+
+    const restar = () => {
+        count > initial ? setCount(count - 1) : alert('no puedes poner menos')
+    }
+
+    const reinicio = () => {
+        setCount(initial);
+    }
+
+
+
+    return (
+    <div className='container fluid'>
+        <div>
+            <h3>PRODUCTO 1</h3>
+            <p>Stock: {stock}</p>
+        </div>
+        <div>
+            <button className='btn btn-secondary' onClick={restar}>-</button>
+            <span className='h3 m-3'id='contador'>{count}</span>
+            <button className='btn btn-secondary' onClick={sumar}>+</button>
+        </div>
+        <button className='btn btn-primary mt-1' onClick={() => {onAdd(count); reinicio() }}>Agregar al carrito</button>
+    </div>
+  )
+}
